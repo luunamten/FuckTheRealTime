7a0d2581ace7a863cb9f8d62eb3680497c60e9a5
Lưu Phước Phương Nam <luunamten@gmail.com>
2020-01-05 15:45:00 +0700

    + Add socket handler and test it

--- app/src/main/java/xxx/fucktherealtime/MainActivity.kt
+++ app/src/main/java/xxx/fucktherealtime/MainActivity.kt
@@ -7,25 +7,31 @@ import android.hardware.SensorEventListener2
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
+import java.net.SocketException
 
 class MainActivity : AppCompatActivity(), SensorEventListener2 {
 
+    private val TAG: String = this.javaClass.name
     private lateinit var mSensorManager: SensorManager
     private lateinit var mSensor: Sensor
-    private var mTimeStamp: Long = 0L
+    private lateinit var mSocketHandler: SocketHandler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
-
+        mSocketHandler = SocketHandler("192.168.0.200", 1234)
     }
 
     override fun onStart() {
         super.onStart()
-        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL)
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_FASTEST)
     }
 
     override fun onStop() {
@@ -38,7 +44,12 @@ class MainActivity : AppCompatActivity(), SensorEventListener2 {
     override fun onFlushCompleted(sensor: Sensor?) {}
 
     override fun onSensorChanged(event: SensorEvent?) {
-        sensorText.text = "X: %f\nY: %f\nZ: %f".format(event!!.values[0], event!!.values[1], event!!.values[2])
-        mTimeStamp = event!!.timestamp
+        val strData: String = "X: %f | Y: %f | Z: %f\n\u0000".format(
+            event!!.values[0],
+            event!!.values[1],
+            event!!.values[2]
+        )
+        sensorText.text = strData
+        mSocketHandler.sendString(strData)
     }
 }
---
+++ app/src/main/java/xxx/fucktherealtime/SocketHandler.kt
@@ -0,0 +1,116 @@
+package xxx.fucktherealtime
+
+import android.os.Handler
+import android.os.HandlerThread
+import android.util.Log
+import java.io.OutputStream
+import java.lang.Exception
+import java.net.InetSocketAddress
+import java.net.Socket
+import java.net.SocketException
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
+class SocketHandler {
+
+    private val FLOAT_BYTES: Int = 4
+    private var mConnectTimeout = 1000
+    private var mInetAddress: InetSocketAddress
+    private var mSocketThread: HandlerThread
+    private var mSocketThreadHandler: Handler
+    private var mTargetByteOrder: ByteOrder = ByteOrder.LITTLE_ENDIAN
+    private lateinit var mSocket: Socket
+
+    constructor(serverIP: String, serverPort: Int) {
+        mInetAddress = InetSocketAddress(serverIP, serverPort)
+        mSocketThread = HandlerThread("Socket")
+        mSocketThread.start()
+        mSocketThreadHandler = Handler(mSocketThread.looper)
+        mSocketThreadHandler.post {
+            connect()
+        }
+    }
+
+    private fun connect() {
+        mSocket = Socket()
+        try {
+            mSocket.connect(mInetAddress, mConnectTimeout)
+        } catch (exp: Exception) {
+            exp.printStackTrace()
+        }
+    }
+
+    fun sendString(data: String) {
+        mSocketThreadHandler.post {
+            try {
+                val stream: OutputStream = mSocket.getOutputStream()
+                stream.write(data.toByteArray())
+            } catch (socketExp: SocketException) {
+                socketExp.printStackTrace()
+                connect()
+            }
+        }
+    }
+
+    fun sendBytes(data: ByteArray) {
+        mSocketThreadHandler.post {
+            try {
+            val stream: OutputStream = mSocket.getOutputStream()
+            stream.write(data)
+            } catch (socketExp: SocketException) {
+                socketExp.printStackTrace()
+                connect()
+            }
+        }
+    }
+
+    fun sendBytes(buffer: ByteBuffer, useTargetByteOrder: Boolean) {
+        mSocketThreadHandler.post {
+            try {
+                val stream: OutputStream = mSocket.getOutputStream()
+                if(useTargetByteOrder) {
+                    buffer.order(mTargetByteOrder)
+                }
+                stream.write(buffer.array())
+            } catch (socketExp: SocketException) {
+                socketExp.printStackTrace()
+                connect()
+            }
+        }
+    }
+
+    fun sendFloat(data: Float) {
+        mSocketThreadHandler.post {
+            try {
+                val stream: OutputStream = mSocket.getOutputStream()
+                val buffer: ByteBuffer = ByteBuffer.allocate(FLOAT_BYTES)
+                buffer.order(mTargetByteOrder)
+                buffer.putFloat(data)
+                stream.write(buffer.array())
+            } catch (socketExp: SocketException) {
+                socketExp.printStackTrace()
+                connect()
+            }
+        }
+    }
+
+    fun setTargetByteOrder(byteOrder: ByteOrder) {
+        mTargetByteOrder = byteOrder
+    }
+
+    fun getTargetByteOrder(): ByteOrder {
+        return mTargetByteOrder
+    }
+
+    fun setConnectTimeout(timeOut: Int) {
+        mConnectTimeout = timeOut
+    }
+
+    fun getConnectTimeout(): Int {
+        return mConnectTimeout
+    }
+
+    fun close() {
+        mSocket.close()
+    }
+}
